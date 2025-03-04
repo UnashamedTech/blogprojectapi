57f91f2fe3bad56fcbb41fdf28e146f8c71ffa54
jerrygirmaa <jerihagbj@gmail.com>
2025-03-04 11:44:47 +0300

    🔧 Modified app.module.ts to update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { PrismaService } from './modules/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+  ],
+  providers: [PrismaService],
   controllers: [],
-  providers: [],
 })
 export class AppModule {}
