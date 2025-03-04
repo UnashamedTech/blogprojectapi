8531acb99d7b984f35248e8a0078be3f02bfb36a
jerrygirmaa <jerihagbj@gmail.com>
2025-03-04 11:46:01 +0300

    ✨ Added common utilities in src/common/

---
+++ src/common/filter/exception-filter.ts
@@ -0,0 +1,53 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+      message =
+        typeof exceptionResponse === 'string'
+          ? exceptionResponse
+          : (exceptionResponse as any).message || message;
+    }
+
+    if (exception instanceof Error) {
+      status = HttpStatus.NOT_FOUND;
+      message = exception.message;
+    }
+
+    if (exception instanceof BadRequestException) {
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+      message =
+        typeof exceptionResponse === 'string'
+          ? exceptionResponse
+          : (exceptionResponse as any).message || message;
+    }
+
+    const formattedResponse = {
+      status: false,
+      error: {
+        code: status,
+        message: message || 'Internal server error',
+      },
+    };
+
+    response.status(status).json(formattedResponse);
+  }
+}
