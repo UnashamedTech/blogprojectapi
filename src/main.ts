f5ebcfe4810d94c4b7dbccd43ad16c4220d428b5
jerrygirmaa <jerihagbj@gmail.com>
2025-03-04 11:45:02 +0300

    🚀 Updated main.ts with improved startup configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/filter/exception-filter';
+import { ResponseInterceptor } from './common/interceptor/response/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: 'Authorization, Content-Type',
+  });
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
