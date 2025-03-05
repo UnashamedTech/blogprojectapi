3dc4ba53b97699873391c1107bc4e92f7de6f799
jerrygirmaa <jerihagbj@gmail.com>
2025-03-05 15:24:40 +0300

    Updated admin module

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BlogModule],
 })
 export class AdminModule {}
