ff0e102427286be595c635f18d376f22fa4cae96
jerrygirmaa <jerihagbj@gmail.com>
2025-03-05 15:26:48 +0300

    Updated role guard

--- src/modules/auth/guard/role/role.guard.ts
+++ src/modules/auth/guard/role/role.guard.ts
@@ -1,22 +1,23 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-import { User } from 'src/modules/admin/user/entities/user.entity';
 import { ROLE_KEY } from '../../auth.decorator';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLE_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    const user: User = context.switchToHttp().getRequest().user;
+    if (!requiredRoles) {
+      return true;
+    }
 
-    return requiredRoles.some((role) => user.name === role);
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return user?.roles?.some((role) => requiredRoles.includes(role));
   }
 }
