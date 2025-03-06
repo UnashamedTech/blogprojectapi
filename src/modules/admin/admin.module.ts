d240fb4b638e005eff57fbae8a82f4fd786fc561
jerrygirmaa <jerihagbj@gmail.com>
2025-03-06 14:00:05 +0300

    Updated Prisma schema and admin module

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { BlogModule } from './blog/blog.module';
+import { InteractionModule } from './interaction/interaction/interaction.module';
+import { CommentModule } from './comment/comment/comment.module';
 
 @Module({
-  imports: [UserModule, BlogModule],
+  imports: [UserModule, BlogModule, InteractionModule, CommentModule],
 })
 export class AdminModule {}
