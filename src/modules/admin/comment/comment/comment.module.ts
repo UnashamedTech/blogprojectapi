6162aefc88116837a1bd6667e7648bed8bf03dc1
jerrygirmaa <jerihagbj@gmail.com>
2025-03-06 14:01:04 +0300

    Added comment module

---
+++ src/modules/admin/comment/comment/comment.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
+import { PrismaModule } from 'src/modules/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '21600s' },
+    }),
+  ],
+  providers: [CommentService],
+  controllers: [CommentController],
+})
+export class CommentModule {}
