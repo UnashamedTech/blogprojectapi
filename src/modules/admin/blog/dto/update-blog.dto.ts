44a05f93a7dd58d9c185ccaa8917752bd56f1681
jerrygirmaa <jerihagbj@gmail.com>
2025-04-17 13:35:14 +0300

    refactor: adjust dto to match updated blog paragraph structure

--- src/modules/admin/blog/dto/update-blog.dto.ts
+++ src/modules/admin/blog/dto/update-blog.dto.ts
@@ -1,18 +1,23 @@
-import { IsString, IsOptional } from 'class-validator';
+import { IsString, IsOptional, IsObject } from 'class-validator';
 
 export class UpdateBlogDto {
+  @IsOptional()
   @IsString()
   title?: string;
 
-  @IsString()
-  content?: string;
+  @IsOptional()
+  @IsObject()
+  content?: Record<string, any>;
 
+  @IsOptional()
   @IsString()
   location?: string;
 
+  @IsOptional()
   @IsString()
-  categoryId: string;
+  categoryId?: string;
 
   @IsOptional()
-  heroImages?: JSON;
+  @IsObject()
+  heroImages?: Record<string, any>;
 }
