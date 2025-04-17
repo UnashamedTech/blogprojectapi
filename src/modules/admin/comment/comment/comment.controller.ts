eb18b3784a0f07b03dde70c9d2776e8d730ee61c
jerrygirmaa <jerihagbj@gmail.com>
2025-04-17 12:35:15 +0300

    Update comment controller

--- src/modules/admin/comment/comment/comment.controller.ts
+++ src/modules/admin/comment/comment/comment.controller.ts
@@ -12,9 +12,10 @@ import {
 import { CommentService } from './comment.service';
 import { Roles } from 'src/modules/auth/auth.decorator';
 import { AuthGuard } from 'src/modules/auth/guard/auth/auth.guard';
+import { RoleGuard } from 'src/modules/auth/guard/role/role.guard';
 
 @Controller('comment')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Roles('OWNER')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
