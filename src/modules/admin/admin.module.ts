5e1c598e571ae2e3e9d713658985e07771d791c4
jerrygirmaa <jerihagbj@gmail.com>
2025-04-17 11:50:13 +0300

    Update admin.module.ts to include Category and Information modules

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,8 +3,17 @@ import { UserModule } from './user/user.module';
 import { BlogModule } from './blog/blog.module';
 import { InteractionModule } from './interaction/interaction/interaction.module';
 import { CommentModule } from './comment/comment/comment.module';
+import { CategoryModule } from './category/category.module';
+import { InformationModule } from './information/information.module';
 
 @Module({
-  imports: [UserModule, BlogModule, InteractionModule, CommentModule],
+  imports: [
+    UserModule,
+    BlogModule,
+    InteractionModule,
+    CommentModule,
+    CategoryModule,
+    InformationModule,
+  ],
 })
 export class AdminModule {}
