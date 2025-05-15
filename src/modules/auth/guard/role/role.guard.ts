3e4268531c027868fd43f53cd5594808681f953a
jerrygirmaa <jerihagbj@gmail.com>
2025-05-15 13:51:05 +0300

    fix: remove unreachable code from RoleGuard

--- src/modules/auth/guard/role/role.guard.ts
+++ src/modules/auth/guard/role/role.guard.ts
@@ -5,6 +5,7 @@ import { ROLE_KEY } from '../../auth.decorator';
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLE_KEY, [
       context.getHandler(),
