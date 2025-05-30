f7376986104a8628ccdcc9bfafc9f6af0e1304b6
jerrygirmaa <jerihagbj@gmail.com>
2025-05-30 15:57:24 +0300

    Register MailService for user invitation

--- src/modules/admin/user/user.module.ts
+++ src/modules/admin/user/user.module.ts
@@ -5,10 +5,11 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { PrismaService } from '../../prisma/prisma.service';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { MailService } from 'src/mail/mail.service';
 @Module({
   imports: [PrismaModule, AuthModule, JwtModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, MailService],
   exports: [UserService],
 })
 export class UserModule {}
