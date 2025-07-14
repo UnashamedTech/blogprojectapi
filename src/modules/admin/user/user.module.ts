e5dcbd6c04253bfedc21c70783ffe55547641bbe
jerrygirmaa <jerihagbj@gmail.com>
2025-07-14 21:30:08 +0300

    Update user.module.ts

--- src/modules/admin/user/user.module.ts
+++ src/modules/admin/user/user.module.ts
@@ -6,8 +6,9 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { MailService } from 'src/mail/mail.service';
+import { MailModule } from 'src/mail/mail.module';
 @Module({
-  imports: [PrismaModule, AuthModule, JwtModule],
+  imports: [PrismaModule, AuthModule, JwtModule, MailModule],
   controllers: [UserController],
   providers: [UserService, PrismaService, MailService],
   exports: [UserService],
