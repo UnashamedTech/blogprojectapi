f541f0c3be52a1d6dedcba9287f16ae684ae6dd8
jerrygirmaa <jerihagbj@gmail.com>
2025-07-14 21:29:39 +0300

    Update contact.module.ts

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,9 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
 import { ContactController } from './contact.controller';
 import { MailService } from 'src/mail/mail.service';
-
+import { MailModule } from 'src/mail/mail.module';
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, MailModule],
   controllers: [ContactController],
   providers: [MailService],
 })
